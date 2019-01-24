fdbfafcee0be943a621f0e97086e908b3c05a09c
Mario Romini <m.romini@teamsystem.com>
2019-01-24 15:59:24 +0100

    Aggiunta crittografia

--- www/hello.js
+++ www/hello.js
@@ -3,5 +3,8 @@
 module.exports = {
     greet: function (name, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    },
+    encrypt: function (email, password, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "encrypt", [email, password]);
     }
 };
